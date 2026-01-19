f26331d74cfa33d00fe77ced594e9a6a2fde1b11
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2026-01-19 21:16:04 +0530

    35:embed-scripts

---
+++ apps/embed/config.ts
@@ -0,0 +1,6 @@
+
+export const EMBED_CONFIG ={
+  WIDGET_URL:import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+  DEFAULT_ORG_ID:"org_34CesZAz6QlAFncDxZj4F2I9FEa",
+  DEFAULT_POSITION:"bottom-right" as const
+}
